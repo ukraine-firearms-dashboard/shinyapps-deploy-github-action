552097f9a204b66d95e95f8c047ec7e83aeb5aaa
ukraine-firearms-dashboard <ukraine.firearms.dashboard@gmail.com>
2025-11-12 19:13:33 +0000

    Update custom_install.R

--- custom_install.R
+++ custom_install.R
@@ -1,7 +1,5 @@
 install.packages('devtools')
-install.packages('gitcreds')
 library(devtools)
-library(gitcreds)
 
 gh_pat <- Sys.getenv("GITHUB_PAT")
 install_github("rstudio/gridlayout", auth_token = gh_pat)
